4dd0e60ca1920fbcb35a3abad8e9204f06f38901
Julia Kim <kimjulia313@gmail.com>
2019-11-17 20:59:59 -0600

    Create remaining tables

--- db/schema.sql
+++ db/schema.sql
@@ -10,5 +10,41 @@ CREATE TABLE staff (
   email VARCHAR(30) NOT NULL,
   automate TINYINT(1) DEFAULT 1,
   findRoom TINYINT(1) DEFAULT 1,
-  PRIMARY KEY(staff_id)
+  PRIMARY KEY (staff_id)
+);
+
+CREATE TABLE students (
+  student_id INT AUTO_INCREMENT,
+  fullName VARCHAR(30) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  program VARCHAR(12) NOT NULL,
+  PRIMARY KEY (student_id)
+);
+
+CREATE TABLE staff_availability (
+  avail_id INT AUTO_INCREMENT,
+  day TINYINT(1) NOT NULL,
+  start TIME NOT NULL,
+  end TIME NOT NULL,
+  staff_id INT NOT NULL,
+  PRIMARY KEY (avail_id),
+  FOREIGN KEY (staff_id)
+    REFERENCES staff (staff_id)
+);
+
+CREATE TABLE office_hours (
+  oh_id INT AUTO_INCREMENT,
+  staff_id INT NOT NULL,
+  student_id INT NOT NULL,
+  date DATE NOT NULL,
+  start TIME NOT NULL,
+  end TIME NOT NULL,
+  topic VARCHAR(255) NOT NULL,
+  room VARCHAR(30),
+  status VARCHAR(10) NOT NULL,
+  PRIMARY KEY (oh_id),
+  FOREIGN KEY (student_id)
+    REFERENCES students (student_id),
+  FOREIGN KEY (staff_id)
+    REFERENCES staff (staff_id)
 );
