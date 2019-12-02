cf54e9d2ff7cf052080eff7fbcf9fcdc82be57f1
Julia Kim <kimjulia313@gmail.com>
2019-12-02 13:42:53 -0600

    Write tests for classes endpoint

--- server/tests/admin.test.js
+++ server/tests/admin.test.js
@@ -1,11 +1,13 @@
-const { exec } = require('child_process');
-const path = require('path');
-const assert = require('chai').assert;
 const request = require('supertest');
-const { user, password } = require('./database/config.js');
+const server = require('../server');
 
 describe('Admin Features', function() {
+  const path = require('path');
+  const { exec } = require('child_process');
+  const { user, password } = require('./database/config.js');
+
   beforeEach(function(done) {
+    // Create and load database
     const schema = path.join(__dirname, 'database', 'schema.sql');
     const seed = path.join(__dirname, 'database', 'seed.sql');
     exec(`mysql -u ${user} -p${password} < ${schema}`, (err) => {
@@ -15,6 +17,35 @@ describe('Admin Features', function() {
       exec(`mysql -u ${user} -p${password} thewiseowls < ${seed}`, (err) => {
         if (err) {
           done(err);
+        }
+        done();
+      });
+    });
+  });
+
+  describe('View current classes', function() {
+    it('When a valid administrator\'s email is provided, expect all currently active courses to be shown', function(done) {
+      request(server)
+        .get('/admin/kk@galvanize.com/classes')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect([{"course": "HRATX 44"}, {"course": "HRATX 45"}, {"course": "MCSP 02"}, {"course": "MCSP 03"}])
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          done();
+        });
+
+    });
+    
+    it('When an invalid email is provided, expect an error', function(done) {
+      request(server)
+      .get('/admin/test@galvanize.com/classes')
+      .expect(403)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
         }
         done();
       });
