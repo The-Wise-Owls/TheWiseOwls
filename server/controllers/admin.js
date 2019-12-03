5e10ed1c1d884196a521741da3dcbae9d4c5835f
Julia Kim <kimjulia313@gmail.com>
2019-12-02 19:47:24 -0600

    Add name and course ID to response

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -9,13 +9,20 @@ exports.getClasses = (req, res) => {
       if (rows.length === 0) {
         res.status(403).send('Invalid email address')
       } else {
-        const courses = [];
+        const classes = [];
         rows.map(row => {
           // Add leading 0 for single-digit cohorts
           row.cohort < 10 ? row.cohort = `0${row.cohort}` : row.cohort;
-          courses.push({course: `${row.name} ${row.cohort}`});
+          classes.push({
+            id: row.class_id,
+            course: `${row.name} ${row.cohort}`
+          });
         });
-        res.status(200).send(courses);
+        const cohorts = {
+          name: rows[0].fullName,
+          classes: classes
+        };
+        res.status(200).send(cohorts);
       }
     })
     .catch(err => res.status(500).send('Error'));
--- server/models/admin.js
+++ server/models/admin.js
@@ -1,7 +1,7 @@
 const db = require('.');
 
 exports.getClassesByEmail = (email) => {
-  const queryString = 'SELECT p.name, c.cohort FROM staff s INNER JOIN staff_programs sp USING (staff_id) INNER JOIN programs p USING (program_id) INNER JOIN classes c ON sp.program_id = c.program WHERE s.email = ?';
+  const queryString = 'SELECT s.fullName, p.name, c.cohort, c.class_id FROM staff s INNER JOIN staff_programs sp USING (staff_id) INNER JOIN programs p USING (program_id) INNER JOIN classes c ON sp.program_id = c.program WHERE s.email = ?';
 
   return db.query(queryString, `${email}`)
     .then((results) => results)
