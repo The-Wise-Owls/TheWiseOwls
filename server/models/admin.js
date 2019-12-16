8773f3f9eff999429053bb5b82128776aad49cd2
Julia Kim <kimjulia313@gmail.com>
2019-12-16 09:51:00 -0600

    Write query for active classes given no parameters

--- server/models/admin.js
+++ server/models/admin.js
@@ -1,5 +1,13 @@
 const db = require('.');
 
+exports.getActiveClasses = () => {
+  const queryString = 'SELECT p.name, c.cohort, c.class_id FROM classes c INNER JOIN programs p ON c.program = p.program_id WHERE c.active = 1;';
+
+  return db.query(queryString)
+    .then(results => results)
+    .catch(err => console.log(`Error querying database for classes: ${err}`));
+};
+
 exports.getClassesByEmail = (email) => {
   const queryString = 'SELECT s.fullName, p.name, c.cohort, c.class_id FROM staff s INNER JOIN staff_programs sp USING (staff_id) INNER JOIN programs p USING (program_id) INNER JOIN classes c ON sp.program_id = c.program WHERE s.email = ?;';
 
